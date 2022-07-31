121f7604b6cf93867129bbc5e158d8c5ba112932
rohitjethoe <rohitchai@icloud.com>
2022-07-31 15:50:57 +0200

    Initial commit: loginUser() function.

--- modules/Reddit.js
+++ modules/Reddit.js
@@ -1,15 +1,37 @@
+/*
+ * Reddit Automation Module
+ * uses @puppeteer
+ * */
+
 const puppeteer = require('puppeteer');
 
 const Reddit = {
     url: 'https://www.reddit.com/login/',
-    browser: null,
-    page: null,
+    karmaUrl: 'https://www.reddit.com/r/FreeKarma4U/',
+    browser: null, 
+    page: null, 
     openBrowser: async () => {
         Reddit.browser = await puppeteer.launch({
             headless: false
         });
         Reddit.page = await Reddit.browser.newPage();
         await Reddit.page.goto(Reddit.url);
+        await Reddit.page.waitForSelector('input#loginUsername');
+    }, 
+    loginUser: async (username, password) => {
+          // reddit@username.
+        await Reddit.page.type(
+            'input[name=username]', 
+            username, 
+            { delay: Math.random * 10 });
+        
+          // reddit@password.
+        await Reddit.page.type(
+            'input[name=password]',
+            password,
+            { delay: Math.random * 10 });
+
+        await Reddit.page.click('button[type=submit]');
     }
 }
 
